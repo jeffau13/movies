34ccc6370d27398ade47496e81bbc22a39d1cf5e
jeffau13 <jeffau13@gmail.com>
2019-03-23 16:28:06 -0400

    using themoviedb api to fetch and display a list of top movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
 class App extends Component {
+  state = {
+    movies: []
+  };
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        'https://api.themoviedb.org/3/discover/movie?api_key=348505c583079cf031367d36be806c53&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {this.state.movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
   }
